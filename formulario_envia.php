e41e9a7b9243ccd97b0476aa2357b50b37499ff7
junior franca <junior-github@hotmail.com>
2014-04-18 02:15:43 -0300

    php001

---
+++ formulario_envia.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sem título</title>
+</head>
+<body>
+
+<form name="nome" id="nome" method="post" action="formulario_recebe.php">
+	<label>Nome:
+    	<input type="text" name="nome" id="nome"/>
+    </label>
+    <br/>
+    
+    <label>Idade:
+    	<input name="idade" id="nome" type="text" maxlength="3" size="5"/>
+    </label>
+    <br/>
+    
+    <label>Sexo:
+    <select name="sexo" id="sexo">
+    	<option></option>
+    	<option value="M">M</option>
+        <option value="F">F</option>
+    </select>
+    </label>
+    <br/>
+    <label>
+    <input type="submit" value="Enviar" name="enviar" id="enviar"/>
+    </label>
+</form>
+
+</body>
+</html>
---
+++ formulario_recebe.php
@@ -0,0 +1,11 @@
+﻿<?php 
+if(isset($_POST['nome'])){
+	$nome = $_POST['nome'];
+	$idade = $_POST['idade'];
+	$sexo = $_POST['sexo'];
+	
+	echo "Nome: $nome <br/>";
+	echo "Idade: $idade <br/>";
+	echo "Sexo: $sexo <br/>";
+}
+?>
